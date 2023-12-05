ac8cc2594e601de9aaa913c1318c496d49fbc4f3
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-12-05 01:18:25 -0300

    Commit 08

--- CalcMedia/Form1.Designer.cs
+++ CalcMedia/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -113,11 +114,21 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "Nota 04";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 398);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox3);
@@ -145,6 +156,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
 

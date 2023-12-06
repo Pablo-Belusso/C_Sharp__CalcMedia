77fcbf8b64629f9b8a2c856c5864c14d6774701b
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-12-06 07:13:10 -0300

    Commit 14

--- CalcMedia/Form1.cs
+++ CalcMedia/Form1.cs
@@ -17,6 +17,11 @@ namespace CalcMedia
             InitializeComponent();
         }
 
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void button1_Click(object sender, EventArgs e) // Calcular
         {
             float soma, media, valor;
@@ -35,7 +40,7 @@ namespace CalcMedia
             }
         }
 
-        private void button2_Click(object sender, EventArgs e) // Limpar
+        private void button2_Click(object sender, EventArgs e)
         {
             foreach (Control controle in this.Controls)
             {
